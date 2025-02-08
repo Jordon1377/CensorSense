3473549f8f2e634584ca6e25467cfacf901d8e60
Jordon1377 <43587817+Jordon1377@users.noreply.github.com>
2025-02-08 17:31:28 -0500

    Added positives and negatives

--- image_cropping_sorting.py
+++ image_cropping_sorting.py
@@ -9,6 +9,12 @@ from Utils.boundingbox_class import BoundingBox
 
 anno_file = "TrainingData/wider_face_annotations.txt"
 
+os.makedirs("Data", exist_ok=True)
+os.makedirs("Data/Negatives", exist_ok=True)
+os.makedirs("Data/Positives", exist_ok=True)
+os.makedirs("Data/Parts", exist_ok=True)
+
+
 annotations = []
 with open(anno_file, 'r') as f:
     lines = f.readlines()
@@ -38,6 +44,10 @@ image_directory = "TrainingData/WIDER_train/images/"
 f_annotations = "Data/filtered_annotations.txt"
 f_n = open(f_annotations, "a")
 
+neg_save_dir = "Data/Negatives/"
+pos_save_dir = "Data/Positives/"
+parts_save_dir = "Data/Parts/"
+
 # Print the parsed annotations
 index = 0
 for anno in annotations:
@@ -59,7 +69,7 @@ for anno in annotations:
 
     negatives = 0
     positives = 0
-    neg_save_dir = "Data/Negatives/"
+    parts = 0
     while negatives < 50:
         size = npr.randint(12, min(width, height) / 2)
 
@@ -77,24 +87,83 @@ for anno in annotations:
                 save_file = os.path.join(neg_save_dir, f"{os.path.basename(image_name)}_{negatives}.jpg")
                 negatives+=1
                 cv2.imwrite(save_file, resized_im)
-                f_n.write(f"{save_file} -1 {(ny, nx)}\n")  # Ensure newline for readability
+                f_n.write(f"{save_file} -1 {(nx, ny, size)}\n")  # Ensure newline for readability
 
     ### POSITIVE FILTERING
+    ### Part Filtering
+    ### Close to face Negative Filtering
 
     for box in bboxes:
         if max(box.w, box.h) < 20: continue
         if box.x1 < 0 or box.x1 > width or box.y1 < 0 or box.y1 > height: continue
 
         n = 0
-        while n < 20:
-            neg_size = npr.randint(12, min(width, height) / 2)
+        while positives < 20 or n < 5:
 
-            nx = npr.randint(min(neg_size, box.x1), width)
-            ny = npr.randint(min(neg_size, box.y1), height)
+            if n < 5:
+                neg_size = npr.randint(12, min(width, height) / 2)
+                x_offset = npr.randint(-neg_size+1, box.w-1)
+                y_offset = npr.randint(-neg_size+1, box.h-1)
 
+                if x_offset + neg_size > width or y_offset + neg_size > height or y_offset < 0 or x_offset < 0:
+                    continue
+                nb = BoundingBox(x_offset, y_offset, neg_size, neg_size, False)
 
+                cropped_im = img[y_offset: y_offset + neg_size, x_offset: x_offset + neg_size, :]
+                resized_im = cv2.resize(cropped_im, (12, 12), interpolation=cv2.INTER_LINEAR)
 
+                i = Utils.helpers.IoU(nb, box)
+                if i < 0.3:
+                    save_file = os.path.join(neg_save_dir, f"{os.path.basename(image_name)}_{negatives}.jpg")
+                    n+=1
+                    negatives+=1
+                    cv2.imwrite(save_file, resized_im)
+                    f_n.write(f"{save_file} -1 {(x_offset, y_offset, neg_size)}\n")  # Ensure newline for readability
 
+            if positives < 20:
+                pos_size = max(12, npr.randint(int(min(box.w, box.h) * 0.95), np.ceil(1.15 * max(box.w, box.h)))) #smaller!!!
+                if box.w < 5 or box.h < 5:
+                    continue
+
+                delta_x = npr.randint(-box.w * 0.1, box.w * 0.1) #Play around with values
+                delta_y = npr.randint(-box.h * 0.1, box.h * 0.1) #Play around with values
+
+                nx1 = int(max(box.x1 + delta_x, 0)) #fix!
+                ny1 = int(max(box.y1 + delta_y, 0))
+
+                nx2 = nx1 + pos_size
+                ny2 = ny1 + pos_size
+
+                offset_x1 = (box.x1 - nx1) / (float(pos_size)/12)
+                offset_y1 = (box.y1 - ny1) / (float(pos_size)/12)
+                offset_x2 = (box.x1 + box.w - nx2) / (float(pos_size)/12)
+                offset_y2 = (box.y1 + box.h - ny2) / (float(pos_size)/12)
+
+                if nx2 > width or ny2 > height or nx1 < 0 or ny1 < 0:
+                    continue 
+
+                nb = BoundingBox(nx1, ny1, pos_size, pos_size, False)
+
+                cropped_im = img[ny1: ny2, nx1: ny2, :]
+                if cropped_im is None or cropped_im.size == 0:
+                    print(f"Error: Cropped image is empty at ({nx1}, {ny1}) to ({nx2}, {ny2}) + ({0}, {0}) to ({width}, {height})")
+                    continue  # Skip this iteration
+                resized_im = cv2.resize(cropped_im, (12, 12), interpolation=cv2.INTER_LINEAR)
+
+                i = Utils.helpers.IoU(nb, box)
+                if i >= 0.65:
+                    save_file = os.path.join(pos_save_dir, f"{os.path.basename(image_name)}_{positives}.jpg")
+                    positives+=1
+                    cv2.imwrite(save_file, resized_im)
+                    f_n.write(f"{save_file} 1 {(nx1, ny1, pos_size)} {offset_x1, offset_y1, offset_x2-offset_x1, offset_y2-offset_y1}\n")  # Ensure newline for readability
+
+                elif i >= 0.4:
+                    save_file = os.path.join(parts_save_dir, f"{os.path.basename(image_name)}_{parts}.jpg") #Maybe do parts?
+                    positives+=1
+                    parts+=1
+                    cv2.imwrite(save_file, resized_im)
+                    f_n.write(f"{save_file} 0 {(nx1, ny1, pos_size)} {offset_x1, offset_y1, offset_x2-offset_x1, offset_y2-offset_y1}\n")  # Ensure newline for readability
+    
     index=index+1
 
 
