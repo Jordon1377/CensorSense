f018dd354cacd30714d43cfad991a9cba61fbded
faizaan5ali <130408625+faizaan5ali@users.noreply.github.com>
2025-02-28 19:23:53 -0500

    Update model.py

--- Model/model.py
+++ Model/model.py
@@ -19,13 +19,14 @@ def create_pnet(input_shape=(12, 12, 3)):
     
     # Face classification branch
     face_class = layers.Conv2D(2, (1, 1), strides=1, padding='valid', name='conv4-1')(x)
+    face_class = layers.Reshape((2,), name="face_class_reshaped")(face_class)  # Flatten output
     face_class = layers.Softmax(name='face_class')(face_class)
     
     # Bounding box regression branch
     bbox_reg = layers.Conv2D(4, (1, 1), strides=1, padding='valid', name='conv4-2')(x)
     
     # Facial landmark localization branch (not sure if needed rn)
-    # landmark_reg = layers.Conv2D(10, (1, 1), strides=1, padding='valid', name='conv4-3')(x)
+    landmark_reg = layers.Conv2D(10, (1, 1), strides=1, padding='valid', name='conv4-3')(x)
     
     model = models.Model(inputs, [face_class, bbox_reg, landmark_reg])
     
